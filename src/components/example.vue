739cc726afa34b1bafc4aaabf2e2bdb3c914dd20
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-07-16 14:28:50 +0300

    Add nested components

--- src/components/example.vue
+++ src/components/example.vue
@@ -2,69 +2,70 @@
 		<dx-data-grid
       keyExpr="orderId"
       :allowColumnReordering="true"
-      :grouping="grouping"
-      :groupPanel="groupPanel"
-      :paging="paging"
-      :selection="selection"
-      :filterRow="filterRow"
       :dataSource="sales"
       :selectedRowKeys="selectedKeys"
-      :columns="columns"
 			>
+      <dx-filter-row :visible="true" />
+      <dx-group-panel :visible="true" />
+      <dx-grouping :autoExpandAll="true" />
+      <dx-paging :pageSize="10" />
+      <dx-selection mode="multiple" />
+      <dx-column 
+        dataField="orderId" 
+        caption="Order ID"
+        :allowSorting="false"
+        :allowFiltering="false"
+        :allowGrouping="false"
+        :allowReordering:="false"
+        :width="100"
+      />
+      <dx-column dataField="city" />
+      <dx-column dataField="country" sortOrder="asc"/>
+      <dx-column dataField="region" :groupIndex="0"/>
+      <dx-column 
+        dataField="date" 
+        dataType="date"
+        selectedFilterOperation=">="
+        filterValue="2013/04/01"
+        :width="150"
+      />
+      <dx-column 
+        dataField="amount" 
+        format="currency"
+        selectedFilterOperation=">="
+        :filterValue="1000"
+        :width="100"
+      />
 		</dx-data-grid>
 </template>
 
 <script>
-import { DxDataGrid } from "devextreme-vue";
+import {
+  DxDataGrid,
+  DxGroupPanel,
+  DxGrouping,
+  DxPaging,
+  DxSelection,
+  DxFilterRow,
+  DxColumn
+} from "devextreme-vue/ui/data-grid";
+
 import { sales, selectedKeys } from "./data.js";
 
 export default {
-  components: { DxDataGrid },
+  components: {
+    DxDataGrid,
+    DxGroupPanel,
+    DxGrouping,
+    DxPaging,
+    DxSelection,
+    DxFilterRow,
+    DxColumn
+  },
   data() {
     return {
       sales,
-      selectedKeys,
-      grouping: { autoExpandAll: true },
-      groupPanel: { visible: true },
-      paging: { pageSize: 10 },
-      selection: { mode: "multiple" },
-      filterRow: { visible: true },
-      columns: [
-        {
-          dataField: "orderId",
-          caption: "Order ID",
-          allowSorting: false,
-          allowFiltering: false,
-          allowGrouping: false,
-          allowReordering: false,
-          width: 100
-        },
-        {
-          dataField: "city"
-        },
-        {
-          dataField: "country",
-          sortOrder: "asc"
-        },
-        {
-          dataField: "region",
-          groupIndex: 0
-        },
-        {
-          dataField: "date",
-          dataType: "date",
-          selectedFilterOperation: ">=",
-          filterValue: "2013/04/01",
-          width: 150
-        },
-        {
-          dataField: "amount",
-          format: "currency",
-          selectedFilterOperation: ">=",
-          filterValue: 1000,
-          width: 100
-        }
-      ]
+      selectedKeys
     };
   }
 };
